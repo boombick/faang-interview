17c40c5cef8c66924324aa177d9a565056a202dd
Andrey Sinitsyn <boombick@yandex.ru>
2022-01-12 21:08:12 +0300

    first task with tree

---
+++ easy/1022_Sum_of_Root_To_Leaf_Binary_Numbers.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/sum-of-root-to-leaf-binary-numbers/
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def sumRootToLeaf(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        def dfs(node, path):
+            if not node: return 0
+
+            path = (path << 1) + node.val
+
+            if not node.left and not node.right:
+                return path
+
+            return dfs(node.left, path) + dfs(node.right, path)
+
+        return dfs(root, 0)
+
